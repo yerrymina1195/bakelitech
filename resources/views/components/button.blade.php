685579fc4b681cbdb2a3e1de8474a3a4c6e2ad69
Mama GDG <mamsogdg@gmail.com>
2023-10-09 18:04:51 +0000

    color/link

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -4,6 +4,6 @@ use App\View\Components\Button;
 
 
     <button type="button"
-        class="py-2.5 px-5 mr-2 h-10 mb-2 text-sm font-medium text-white focus:outline-none bg-greenbakeli border-orange-500 border-r-4 hover:bg-green-700 hover:text-white-700 focus:z-10 focus:ring-4 focus:ring-gray-200 ">
+        class="py-2.5 px-5 mr-2 h-10 mb-2 text-sm font-medium text-white focus:outline-none bg-greenbakeli border-yellow-300 border-r-4 hover:bg-green-700 hover:text-white-700 focus:z-10 focus:ring-4 focus:ring-gray-200 ">
         {{ $attributes['title'] }} 
     </button>
